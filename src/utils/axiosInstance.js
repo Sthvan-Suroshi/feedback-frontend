7d3e12b22f3f3407cf164fe33209799236829d7f
sthvan31 <sthvan.suroshi@wundrsight.com>
2025-04-09 00:24:31 +0530

    feat: delete images

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.withCredentials = true;
-axiosInstance.defaults.baseURL = "http://localhost:8000/api/v1";
+axiosInstance.defaults.baseURL = "https://be1.sthvansuroshi.online/api/v1";
 
 export default axiosInstance;
