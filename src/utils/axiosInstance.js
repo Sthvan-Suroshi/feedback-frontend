587d55777b8e000932641b47cca4e420722dab74
sthvan31 <sthvan.suroshi@wundrsight.com>
2025-04-09 00:49:14 +0530

    fix: fix import

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,5 +4,6 @@ const axiosInstance = axios.create();
 
 axiosInstance.defaults.withCredentials = true;
 axiosInstance.defaults.baseURL = "https://be1.sthvansuroshi.online/api/v1";
+// axiosInstance.defaults.baseURL = "http://localhost:8000/api/v1";
 
 export default axiosInstance;
